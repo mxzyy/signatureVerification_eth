fc5566944bdd39461fd290a6eb4f4f738c42095a
mxzyy <hilmyp53@gmail.com>
2025-02-02 00:26:19 +0700

    Add verify signature test

--- contracts/SignatureVerification.sol
+++ contracts/SignatureVerification.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.26;
 contract SignatureVerification {
     // Fungsi untuk memverifikasi tanda tangan
     function verify(
-        address _signer,
+        //address _signer,
         bytes32 _messageHash,
         uint8 _v,
         bytes32 _r,
         bytes32 _s
-    ) public pure returns (bool) {
+    ) public pure returns (address) {
         // Memulihkan alamat penandatangan dari hash dan tanda tangan
         address recoveredAddress = ecrecover(_messageHash, _v, _r, _s);
 
         // Membandingkan alamat yang dipulihkan dengan alamat yang diklaim
-        return recoveredAddress == _signer;
+        return recoveredAddress;
     }
 
     // Fungsi untuk menghasilkan hash pesan
--- test/SignatureVerification.js
+++ test/SignatureVerification.js
@@ -24,21 +24,48 @@ describe("Signature Verification", function() {
             console.log("    Contract Address : ", owner.address);
         });
 
-        it("Testing to hash a message!", async function () {
+        
+    });
+
+    describe("Testing Function", function () {
+        it("Testing getMessageHash to hash a message.", async function () {
             const {sv, _} = await loadFixture(deployContract);
             const message = "Testing";
             const solHash =  await sv.getMessageHash(message);
-            expect(solHash).to.exist;
-            console.log("    Hashed message : ",solHash);
+            const hash = ethers.id(message);
+            expect(solHash).to.equal(hash);
+            
         });
 
-        it("Testing to parse the hash message!", async function () {
+        it("Testing getEthSignedMessageHash to parse the hash message.", async function () {
             const {sv, _} = await loadFixture(deployContract);
             const message = "Testing";
             const solHash =  await sv.getMessageHash(message);
             const parseHash = await sv.getEthSignedMessageHash(solHash);
             expect(parseHash).to.exist;
             console.log("    Parsed message : ", parseHash);
+        });
+
+        it("Testing verify to verify hashed message.", async function () {
+            const {sv, _} = await loadFixture(deployContract);
+            const provider = ethers.getDefaultProvider("http://localhost:8545/");
+            const signer = new ethers.Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", provider);
+            const signer_address = signer.getAddress();
+            const message = "Testing";
+
+            const solHash =  await ethers.hashMessage(message);
+            const parseHash = await ethers.toUtf8Bytes(solHash);
+            const signedMsg = await signer.signMessage(parseHash);
+            //const sig       = await ethers.Signature.from(signedMsg);
+
+            //const testSignedMsg = await sv.verify( parseHash, sig.v, sig.r, sig.s);    
+            //expect(testSignedMsg).to.exist;
+            const verify = ethers.verifyMessage(ethers.hashMessage(message), signedMsg);
+            console.log("Recovered Address:", verify);
+            //console.log("    Signed Message Verification result : ", verify);
+            //console.log("signer addy : ", signer_address);
+            //console.log(parseHash);
+            
         });
     });
   });
