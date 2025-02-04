742f97fb1fcc6c63b1e3013889a9b751735803db
mxzyy <hilmyp53@gmail.com>
2025-02-04 22:12:19 +0700

    Add Connect Wallet Button

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,32 @@
+'use client'
 import Head from "next/head";
 import Image from "next/image";
 import Account from "./components/account";
 import Form from "./components/form";
 import Navbar from "./components/navbar";
+import React, { useState, useEffect } from "react";
+import { requestAccount } from "./utils/contractServices";
 
 export default function Home() {
+  const [account, setAccount] = useState(null);
+  useEffect(() => {
+    const fetchCurAccount = async () => {
+      const account = await requestAccount();
+      setAccount(account);
+    };
+    fetchCurAccount();
+  }, []);
+
+  useEffect(() => {
+    const handleAccountChanged = (newAccounts) =>
+      setAccount(newAccounts.length > 0 ? newAccounts[0] : null);
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", handleAccountChanged);
+    }
+    return () => {
+      window.ethereum?.removeListener("accountsChanged", handleAccountChanged);
+    };
+  });
   return (
     <>
       <Head>
@@ -12,11 +34,11 @@ export default function Home() {
         <meta name="author" content="mxzyy" />
         <meta name="description" content="Test Client dApp" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/x-icon" href="https://ethereum.org/images/favicon.png"/>
+        <link rel="icon" type="image/x-icon" href="https://ethereum.org/images/favicon.png" />
       </Head>
-      <Navbar/>
-      <Account/>
-      <Form/>
+      <Navbar />
+      <Account setAccount={setAccount} accountAddr={account}/>
+      <Form />
     </>
   );
 }
